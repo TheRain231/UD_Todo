99d34adc76d4c70738a5e97d437fbfb5f4925208
Андрей Степанов <therain3164@yandex.ru>
2025-02-26 09:18:29 +0300

    Initial Commit

---
+++ UD_Todo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UD_Todo
+//
+//  Created by Андрей Степанов on 26.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UD_Todo/UD_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UD_TodoApp.swift
+//  UD_Todo
+//
+//  Created by Андрей Степанов on 26.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct UD_TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
